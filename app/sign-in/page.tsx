29bfd9d7bb0324208ca0954ab03c04f722473bb0
ABDO <abdo@ABDOs-MacBook-Air.local>
2026-01-20 18:46:22 +0200

    first project

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { SignIn } from "@stackframe/stack";
+export default function SignInPage() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center ">
+      <SignIn/>
+    </div>
+  );
+}
